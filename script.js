940d36e9d3613a990602145a9c23d1f6974f272c
Majelvey <mckelvey.andy@gmail.com>
2022-05-14 14:06:47 -0400

    changed logic - created var passwordBase for converting array userChoices to a string - rewrote writePassword - addition of substring

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ var alphaLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 var alphaUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var symbols = ["!", "*", "+", "-", "?", "@", "^", "_", "~"];
-var userChoices;
-
-
-
+var userChoices = [];
+var password = ''
 // generatePassword function
 function generatePassword() {
-  var passwordLength = (prompt("How many characters would you like your password? Choose between 8 and 128"));
+  var passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  var passwordLengthInteger = parseInt(passwordLength)
   if (!passwordLength) {
     alert("You haven't supplied a value");
   } else if (passwordLength < 8 || passwordLength > 128) {
@@ -23,46 +22,48 @@ function generatePassword() {
     confirmUppercase = confirm("Will this contain Uppercase letters?");
     confirmLowercase = confirm("Will this contain Lowercase letters?");
   };
-  // If user selects no options
-  if (!confirmSymbols && !confirmNumber && !confirmUppercase && !confirmLowercase) {
-    userChoices = alert("You must choose a criteria!");
-    // If user selects all four confirms
-  } else if (confirmNumbers && confirmSymbols && confirmUppercase && confirmLowercase) {
-    userChoices = alphaLowercase.concat(alphaUppercase, numbers, symbols);
-    // If user selects confirmNumber, confirmCharacter, and confirmUppercase
-  } else if (confirmNumbers && confirmSymbols && confirmUppercase) {
-    userChoices = numbers.concat(symbols, alphaUppercase);
-    // If user selects confirmNumbers, confirmSymbols, and confirmLowercase
-  } else if (confirmNumbers && confirmSymbols && confirmLowercase) {
-    userChoices = numbers.concat(symbols, alphaLowercase);
-  };
-  var password = [];
-  for (var i = 0; i < userChoices; i++) {
-    var getPassword = userChoices[Math.floor(Math.random() * userChoices.length)];
-    console.log(getPassword);
+
+  if (confirmLowercase) {
+    userChoices = userChoices.concat(alphaLowercase);
   }
-  
-  return getPassword;
+
+  if (confirmUppercase) {
+    userChoices = userChoices.concat(alphaUppercase);
+  }
+
+  if (confirmNumbers) {
+    userChoices = userChoices.concat(numbers);
+  }
+
+  if (confirmSymbols) {
+    userChoices = userChoices.concat(symbols);
+    
+  }
+  var passwordBase = userChoices.join("");
+  for (var i = 0; i < passwordLengthInteger; i++) {
+    var randomNumber = Math.floor(Math.random() * passwordBase.length);
+    password += passwordBase.substring(randomNumber, randomNumber +1)
+  };
+
+  console.log(password);
+  writePassword(password)
+  return password;
 };
 
+//1. Prompt password length; Definte passowrd length, check if it is between 8 and 128, promptLowercase()
 
-
-
-
+//promntLowercase . Check for yes/no lowercase. function called prompt upperCase
 
 
 // Write password to the #password input
-function writePassword() {
-  console.log("Is it working?")
-  var password = generatePassword();
+function writePassword(text) {
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = text;
 };
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-writePassword()
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
 
 
 
